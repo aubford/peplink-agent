6e69c33d0519f22ae1aae84471a687ffa6f2efd4
Aubrey <MusicAubrey@gmail.com>
2024-12-16 23:01:23 -0500

    fix website extractor

--- extract/website/website_extractor.py
+++ extract/website/website_extractor.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import RecursiveUrlLoader
 from bs4 import BeautifulSoup
 from extract.base_extractor import BaseExtractor, Ldoc
 from util.util import serialize_document
+import time
 
 
 class WebsiteExtractor(BaseExtractor):
@@ -15,10 +16,10 @@ class WebsiteExtractor(BaseExtractor):
             use_async=False,
             max_depth=max_depth,
             extractor=self._extract_content,
-            # prevent_outside=True,
-            # headers={
-            #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-            # },
+            prevent_outside=True,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+            },
             exclude_dirs=(
                 "/cart", "/login", "/account", "/search", "/pdf", ".pdf", "/wp-admin/", "/wp-includes/", "/cgi-bin/",
                 "/private/", "/tmp/", "/admin/", "/login/", "/user/", "/dashboard/", "/scripts/", "/styles/")
@@ -41,4 +42,5 @@ class WebsiteExtractor(BaseExtractor):
         stream_key = self.start_stream(Ldoc, identifier=self.file_id)
         for doc in self.loader.lazy_load():
             self.stream_item(serialize_document(doc), stream_key)
+            time.sleep(1)  # Add 1 second delay between iterations
         self.end_stream(stream_key)
