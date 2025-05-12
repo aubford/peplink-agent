be7a1479d211fc0a94168f189df8e84cdff23aaa
Aubrey <MusicAubrey@gmail.com>
2025-05-11 19:20:17 -0700

    cohere rerank results

--- evals/runs/mutate_summary.py
+++ evals/runs/mutate_summary.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 df = pd.read_parquet('./test_runs_summary.parquet')
-df_reordered = df.loc[[3, 1, 2, 0, 4, 5]].set_index('run_name')
-df_reordered.to_parquet('./test_runs_summary_out.parquet')
+df_reordered = df.loc[[1, 3, 0, 2, 4, 5]]
+df_reordered.to_parquet('./test_runs_summary_out.parquet', index=False)
